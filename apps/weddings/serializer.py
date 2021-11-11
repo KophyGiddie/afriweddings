c79317b8393a7dbb68ca015597a193fae555d6c2
Gideon Boateng <gidboa@gmail.com>
2021-11-11 15:55:33 +0000

    added admins m2m field to weddings model

--- apps/weddings/serializer.py
+++ apps/weddings/serializer.py
@@ -9,6 +9,8 @@ class WeddingSerializer(serializers.Serializer):
     hashtag = serializers.CharField()
     partner_first_name = serializers.CharField()
     currency = serializers.CharField()
+    country = serializers.CharField()
+    city = serializers.CharField()
     partner_last_name = serializers.CharField()
     venue = serializers.CharField()
     budget = serializers.DecimalField(max_digits=15, decimal_places=2)
