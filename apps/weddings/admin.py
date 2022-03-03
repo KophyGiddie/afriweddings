d0c1a090403a707f31f902a05824a56eb6bf5685
Gideon Boateng <gidboa@gmail.com>
2022-03-03 11:49:53 +0000

    fixed base url issue in live

--- apps/weddings/admin.py
+++ apps/weddings/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from apps.weddings.models import *
 
-"""
-Register your models here to appear in Admin Backend
-
-"""
 admin.site.register(Wedding)
 admin.site.register(WeddingTeam)
 admin.site.register(WeddingRole)
